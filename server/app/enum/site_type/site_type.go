c38a4630b6497c75f62be12cebf83482d497628c
SinKingCloud <sinkingcloud@qq.com>
2026-08-23 10:56:42 +0800

    修正

--- server/app/enum/site_type/site_type.go
+++ server/app/enum/site_type/site_type.go
@@ -1,16 +1,18 @@
 package site_type
 
 const (
-	Static = iota // 静态网站
-	Proxy         // 反向代理
-	PHP           // PHP网站
+	Static  = iota // 静态网站
+	Proxy          // 反向代理
+	General        // 通用网站
+	PHP            // PHP网站
 )
 
 // Map 网站类型数据
 func Map() map[int]string {
 	return map[int]string{
-		Static: "静态网站",
-		Proxy:  "反向代理",
-		PHP:    "PHP网站",
+		Static:  "静态网站",
+		Proxy:   "反向代理",
+		General: "通用网站",
+		PHP:     "PHP网站",
 	}
 }
